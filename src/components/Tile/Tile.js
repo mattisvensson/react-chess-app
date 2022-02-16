d6167bdf887ec816954953ab1c3f29262bf12813
Matti Svensson <matti..58@web.de>
2022-02-16 10:28:21 +0100

    setActivePiece Object bug

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,7 +3,7 @@ import './Tile.css';
 
 function Tile (props) {
     return (
-        <div className={`tile ${props.number % 2 === 0 ? "light" : "dark"}`}>
+        <div className={`tile ${props.number % 2 === 0 ? "light" : "dark"} ${props.id}`}>
             {props.image.indexOf("undefined") > -1 ? "" : <div className="piece" style={{backgroundImage: `url(${props.image})`}}></div> }
         </div>
     )
